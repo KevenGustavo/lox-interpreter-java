ceea637282915d95f963de98cf172e72f22e67c6
KG.DSG <kevengustavo2012@gmail.com>
2025-12-13 13:58:09 -0300

    Resolver Class

--- src/main/java/com/lox/Stmt.java
+++ src/main/java/com/lox/Stmt.java
@@ -6,48 +6,53 @@ abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
-    R visitPrintStmt(Print stmt);
-    R visitVarStmt(Var stmt);
+    R visitFunctionStmt(Function stmt);
     R visitIfStmt(If stmt);
+    R visitPrintStmt(Print stmt);
+    R visitReturnStmt(Return stmt);
+    R visitVarStmt(Var stmt);
+    R visitWhileStmt(While stmt);
   }
-
   static class Block extends Stmt {
     final List<Stmt> statements;
-    Block(List<Stmt> statements) { this.statements = statements; }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) { return visitor.visitBlockStmt(this); }
-  }
-
-  static class Expression extends Stmt {
-    final Expr expression;
-    Expression(Expr expression) { this.expression = expression; }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) { return visitor.visitExpressionStmt(this); }
-  }
-
-  static class Print extends Stmt {
-    final Expr expression;
-    Print(Expr expression) { this.expression = expression; }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) { return visitor.visitPrintStmt(this); }
-  }
-
-  static class Var extends Stmt {
-    final Token name;
-    final Expr initializer;
-
-    Var(Token name, Expr initializer) {
-      this.name = name;
-      this.initializer = initializer;
+    Block(List<Stmt> statements) {
+      this.statements = statements;
     }
 
     @Override
-    <R> R accept(Visitor<R> visitor) { return visitor.visitVarStmt(this); }
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBlockStmt(this);
+    }
   }
+  static class Expression extends Stmt {
+    final Expr expression;
 
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+  }
+  static class Function extends Stmt {
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
+
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+    }
+  }
   static class If extends Stmt {
     final Expr condition;
     final Stmt thenBranch;
@@ -60,7 +65,63 @@ abstract class Stmt {
     }
 
     @Override
-    <R> R accept(Visitor<R> visitor) { return visitor.visitIfStmt(this); }
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+  }
+  static class Print extends Stmt {
+    final Expr expression;
+
+    Print(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+  }
+  static class Return extends Stmt {
+    final Token keyword;
+    final Expr value;
+
+    Return(Token keyword, Expr value) {
+      this.keyword = keyword;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitReturnStmt(this);
+    }
+  }
+  static class Var extends Stmt {
+    final Token name;
+    final Expr initializer;
+
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+  }
+  static class While extends Stmt {
+    final Expr condition;
+    final Stmt body;
+
+    While(Expr condition, Stmt body) {
+      this.condition = condition;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitWhileStmt(this);
+    }
   }
 
   abstract <R> R accept(Visitor<R> visitor);
--- src/main/java/com/lox/tool/GenerateAst.java
+++ src/main/java/com/lox/tool/GenerateAst.java
@@ -45,7 +45,7 @@ public class GenerateAst {
     String path = outputDir + "/" + baseName + ".java";
     PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-    writer.println("package com.craftinginterpreters.lox;");
+    writer.println("package com.lox;");
     writer.println();
     writer.println("import java.util.List;");
     writer.println();
