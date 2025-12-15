2474f4f83f9e8c04d49ef9341ccc84ed6e47d5d2
arthurswntg2000 <arthur.sampaio@discente.ufma.br>
2025-12-15 20:41:12 -0300

    Atualizando

--- src/main/java/com/lox/LoxCallable.java
+++ src/main/java/com/lox/LoxCallable.java
@@ -2,7 +2,7 @@ package com.lox;
 
 import java.util.List;
 
-public class LoxCallable {
-  int arity();
-  Object call(Interpreter interpreter, List<Object> arguments);
+interface LoxCallable {
+    int arity();
+    Object call(Interpreter interpreter, List<Object> arguments);
 }
